9caf1ec495f6a54e404020cfd390883f7f36ea3a
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2023-04-29 01:30:27 +0530

    feat: http response

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod client;
 fn main() {
   let server_handle = server::listen();
 
-  client::make_request();
+  // client::make_request();
 
   server_handle.join().unwrap();
 } 
--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,31 @@
 use std::{thread::{self, JoinHandle}, net::{TcpStream, Shutdown, TcpListener}, io::{Read, Write}};
 
-fn handle_client(mut stream: TcpStream) {
-  let mut data = [0 as u8; 50]; // using 50 byte buffer
-  while match stream.read(&mut data) {
-    Ok(size) => {
-      // echo everything!
-      stream.write(&data[0..size]).unwrap();
-      true
+fn handle_read(mut stream: &TcpStream) -> [u8; 4096] {
+  let mut buf = [0u8 ;4096];
+  match stream.read(&mut buf) {
+    Ok(_) => {
+      let req_str = String::from_utf8_lossy(&buf);
+      println!("Received Req: {}", req_str);
+      buf
     },
-    Err(_) => {
-      println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-      stream.shutdown(Shutdown::Both).unwrap();
-      false
+    Err(e) => panic!("Unable to read stream: {}", e),
     }
-  } {}
 }
 
+fn handle_write(mut stream: &TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body><h1>Hello world</h1></body></html>\r\n";
+    match stream.write(response) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    handle_read(&stream);
+    handle_write(&stream);
+
+    // stream.shutdown(Shutdown::Both).unwrap();    
+}
 
 
 pub fn listen() -> JoinHandle<()> {
